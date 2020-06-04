ca42e2b3b68d32a024614df061b2444f9d0b188e
alivane <vaneg643@gmail.com>
2020-06-04 00:27:32 -0400

    add url

--- src/services/ConnectionHttp.js
+++ src/services/ConnectionHttp.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// baseURL: "https://wgzybrev3b.execute-api.us-east-2.amazonaws.com/dev",
 export default axios.create({
-  baseURL: "https://wgzybrev3b.execute-api.us-east-2.amazonaws.com/dev",
+  baseURL: "http://localhost:3000",
 });
